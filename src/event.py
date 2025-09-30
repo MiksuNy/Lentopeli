294a0cc1ef7cef1f7ad6975a54b7264525f8acad
HEPOSHEIKKI <otto.varteva@gmail.com>
2025-09-30 14:01:14 +0300

    Implement Command, break event loop, refactor

--- src/event.py
+++ src/event.py
@@ -1,8 +1,5 @@
 from enum import Enum
-
-class Event(Enum):
-    QUIT_GAME = 0
-    NEXT_TURN = 1
+from command import Command
 
 class EventManager:
     def __init__(self):
@@ -13,10 +10,3 @@ class EventManager:
 
     def pop_queue(self):
         return self.event_queue.pop(0)
-
-    def queue_event_from_string(self, string):
-        match string:
-            case "quit":
-                self.append_queue(Event.QUIT_GAME)
-            case "next":
-                self.append_queue(Event.NEXT_TURN)
